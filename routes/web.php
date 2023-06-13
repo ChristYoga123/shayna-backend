9e3b1faf0a314a3fad08ab442f6498e05eaaa368
ChristYoga123 <christianuswibisono@gmail.com>
2023-06-13 21:02:10 +0700

    feat:slicing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/* Auth */
+
+Route::prefix("admin")->name("admin.")->group(function () {
+    Route::get('/', [DashboardController::class, "index"]);
+});
+
+/* non Auth */
+Route::prefix("admin")->name("admin.")->group(function () {
+    Route::get("login", [AuthController::class, "index"])->name("login.index");
 });
